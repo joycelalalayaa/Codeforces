5676261bf4d950d99857d5d45b1682d4567c35fc
Joyce Lynn <joycelyuz@gmail.com>
2024-03-04 19:49:01 -0800

    03/04/24

---
+++ CF805BPolycarpWritesAStringFromMemory.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class CF805BPolycarpWritesAStringFromMemory {
+    public static void main(String[] args) {
+        Scanner fs = new Scanner(System.in);
+        int testcases = fs.nextInt();
+        for (int tc = 0; tc < testcases; tc++) {
+            String input = fs.next();
+            HashSet<Character> arr = new HashSet<>();
+            int ans = 0;
+            for (int i = 0; i < input.length()-1; i++) {
+                arr.add(input.charAt(i));
+                if (arr.size() == 3 && !arr.contains(input.charAt(i+1))) {
+                    ans++;
+                    arr.clear(); 
+                }
+            }
+            ans++; 
+            System.out.println(ans);
+        }
+    }
+}
