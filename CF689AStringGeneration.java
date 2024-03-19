933df7f686930092a443c6afbf0ff3daaa8fcad1
Joyce Lynn <joycelyuz@gmail.com>
2024-03-18 21:01:12 -0700

    edited 03/18/24

--- CF689AStringGeneration.java
+++ CF689AStringGeneration.java
@@ -1,18 +1,24 @@
 import java.util.*;
 
+/*
+
+arr.len = 10
+i     i     i     i
+0 1 2 3 4 5 6 7 8 9
+a b c a b c a b c 0
+ */
 public class CF689AStringGeneration {
     public static void main(String[] args) {
         Scanner fs = new Scanner(System.in);
         int testcases = fs.nextInt();
         for (int tc = 0; tc < testcases; tc++) {
             int len = fs.nextInt(), panLen = fs.nextInt();
-            char[] arr = "bcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabca"
-                    .toCharArray();
-            for (int i = 0; i < panLen; i++) {
-                System.out.print('a');
-            }
-            for (int i = 0; i < len - panLen; i++) {
-                System.out.print(arr[i]);
+            // char[] arr = new char[1000];
+            // for (int i = 0; i < arr.length; i++) {
+            //     arr[i] = (char) ('a' + i % 3);
+            // }
+            for (int i = 0; i < len; i++) {
+                System.out.print((char)('a' + i % 3));
             }
             System.out.println();
         }
