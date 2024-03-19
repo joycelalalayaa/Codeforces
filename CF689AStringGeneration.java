f493a71780a0b23c03933f29352d5be1b56b926b
Joyce Lynn <joycelyuz@gmail.com>
2024-03-18 19:27:32 -0700

    03/18/2024

---
+++ CF689AStringGeneration.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class CF689AStringGeneration {
+    public static void main(String[] args) {
+        Scanner fs = new Scanner(System.in);
+        int testcases = fs.nextInt();
+        for (int tc = 0; tc < testcases; tc++) {
+            int len = fs.nextInt(), panLen = fs.nextInt();
+            char[] arr = "bcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabca"
+                    .toCharArray();
+            for (int i = 0; i < panLen; i++) {
+                System.out.print('a');
+            }
+            for (int i = 0; i < len - panLen; i++) {
+                System.out.print(arr[i]);
+            }
+            System.out.println();
+        }
+    }
+}
