23b0ba3567ff0dc021bb2d173b10a0cdc0905d5c
Joyce Lynn <joycelyuz@gmail.com>
2024-03-23 18:34:54 -0700

    03/23/24

---
+++ CF833ATheUltimateSquare.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CF833ATheUltimateSquare {
+    public static void main(String[] args) {
+        Scanner fs = new Scanner(System.in);
+        int testcases = fs.nextInt();
+        for (int tc = 0; tc < testcases; tc++) {
+            int num = fs.nextInt();
+            System.out.println((num+1)/2);
+        }
+    }
+}
